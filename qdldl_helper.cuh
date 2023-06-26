7e3302e8964b0dd9d844b1952fba894f226ff791
EmreAdabag <emreadabag@gmail.com>
2023-06-25 22:34:27 -0400

    doesnt work yet

--- qdldl_helper.cuh
+++ qdldl_helper.cuh
@@ -256,6 +256,152 @@ double qdldl_solve_schur(uint32_t state_size, uint32_t knot_points, float *d_S, 
 
 }
 
+///TODO: pass in allocated mem
+__host__
+void qdldl_solve_schur(uint32_t state_size, uint32_t knot_points, int *h_col_ptr, int *h_row_ind, float *h_val, float *h_gamma, float *h_lambda){
+
+	
+
+    const QDLDL_int An = state_size*knot_points;
+    const QDLDL_int *Ap = (QDLDL_int *) h_col_ptr;
+    const QDLDL_int *Ai = (QDLDL_int *) h_row_ind; 
+    const QDLDL_float *Ax = (QDLDL_float *)  h_val;
+    const QDLDL_float *b =  (QDLDL_float *) h_gamma;
+
+
+
+    QDLDL_int i;
+
+    //data for L and D factors
+	QDLDL_int Ln = An;
+	QDLDL_int *Lp;
+	QDLDL_int *Li;
+	QDLDL_float *Lx;
+	QDLDL_float *D;
+	QDLDL_float *Dinv;
+
+	//data for elim tree calculation
+	QDLDL_int *etree;
+	QDLDL_int *Lnz;
+	QDLDL_int  sumLnz;
+
+	//working data for factorisation
+	QDLDL_int   *iwork;
+	QDLDL_bool  *bwork;
+	QDLDL_float *fwork;
+	//Data for results of A\b
+	QDLDL_float *x = h_lambda;
+
+
+	/*--------------------------------
+	* pre-factorisation memory allocations
+	*---------------------------------*/
+
+	//These can happen *before* the etree is calculated
+	//since the sizes are not sparsity pattern specific
+
+	//For the elimination tree
+	etree = (QDLDL_int*)malloc(sizeof(QDLDL_int)*An);
+	Lnz   = (QDLDL_int*)malloc(sizeof(QDLDL_int)*An);
+
+	//For the L factors.   Li and Lx are sparsity dependent
+	//so must be done after the etree is constructed
+	Lp    = (QDLDL_int*)malloc(sizeof(QDLDL_int)*(An+1));
+	D     = (QDLDL_float*)malloc(sizeof(QDLDL_float)*An);
+	Dinv  = (QDLDL_float*)malloc(sizeof(QDLDL_float)*An);
+
+	//Working memory.  Note that both the etree and factor
+	//calls requires a working vector of QDLDL_int, with
+	//the factor function requiring 3*An elements and the
+	//etree only An elements.   Just allocate the larger
+	//amount here and use it in both places
+	iwork = (QDLDL_int*)malloc(sizeof(QDLDL_int)*(3*An));
+	bwork = (QDLDL_bool*)malloc(sizeof(QDLDL_bool)*An);
+	fwork = (QDLDL_float*)malloc(sizeof(QDLDL_float)*An);
+	/*--------------------------------
+	* elimination tree calculation
+	*---------------------------------*/
+
+	sumLnz = QDLDL_etree(An,Ap,Ai,iwork,Lnz,etree);
+	std::cout << "1\n";
+	/*--------------------------------
+	* LDL factorisation
+	*---------------------------------*/
+
+	//First allocate memory for Li and Lx
+	Li    = (QDLDL_int*)malloc(sizeof(QDLDL_int)*sumLnz);
+	Lx    = (QDLDL_float*)malloc(sizeof(QDLDL_float)*sumLnz);
+
+	//now factor
+	QDLDL_factor(An,Ap,Ai,Ax,Lp,Li,Lx,D,Dinv,Lnz,etree,bwork,iwork,fwork);
+	std::cout << "2\n";
+	/*--------------------------------
+	* solve
+	*---------------------------------*/
+	// x = (QDLDL_float*)malloc(sizeof(QDLDL_float)*An);
+
+	//when solving A\b, start with x = b
+	for(i=0;i < Ln; i++) x[i] = b[i];
+	std::cout << "2.5\n";
+
+	QDLDL_solve(Ln,Lp,Li,Lx,Dinv,x);
+	std::cout << "3\n";
+
+	/*--------------------------------
+	* print factors and solution
+	*---------------------------------*/
+/*	printf("\n");
+	printf("A (CSC format):\n");
+	print_line();
+	print_arrayi(Ap, An + 1, "A.p");
+	print_arrayi(Ai, Ap[An], "A.i");
+	print_arrayf(Ax, Ap[An], "A.x");
+	printf("\n\n");
+
+	printf("elimination tree:\n");
+	print_line();
+	print_arrayi(etree, Ln, "etree");
+	print_arrayi(Lnz, Ln, "Lnz");
+	printf("\n\n");
+
+	printf("L (CSC format):\n");
+	print_line();
+	print_arrayi(Lp, Ln + 1, "L.p");
+	print_arrayi(Li, Lp[Ln], "L.i");
+	print_arrayf(Lx, Lp[Ln], "L.x");
+	printf("\n\n");
+
+	printf("D:\n");
+	print_line();
+	print_arrayf(D, An,    "diag(D)     ");
+	print_arrayf(Dinv, An, "diag(D^{-1})");
+	printf("\n\n");
+
+	printf("solve results:\n");
+	print_line();
+	print_arrayf(b, An, "b");
+	print_arrayf(x, An, "A\\b");
+	printf("\n\n");
+*/
+
+	/*--------------------------------
+	* clean up
+	*---------------------------------*/
+	free(Lp);
+	free(Li);
+	free(Lx);
+	free(D);
+	free(Dinv);
+	free(etree);
+	free(Lnz);
+	free(iwork);
+	free(bwork);
+	free(fwork);
+	// free(x);
+
+
+}
+
 
 
 }
