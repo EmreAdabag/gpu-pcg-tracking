0cbae845eb65c508f5c7e8818502530a36a7b84c
William Gerard <whgerard1@gmail.com>
2023-09-01 09:07:36 -0400

    Update test_croc to reproduce float error

--- test_croc.cpp
+++ test_croc.cpp
@@ -55,28 +55,24 @@ int main(int argc, char **argv)
 	// NOTE: parsing the URDF only works with doubles, if we need to use floats we need to parse first and then cast
 	// https://github.com/stack-of-tasks/pinocchio/issues/2037
 	pinocchio::ModelTpl<double> model_double;
-	// ModelTpl<double> model;
+	// ModelTpl<float> model;
 	pinocchio::urdf::buildModel(urdf_filename, model_double);
 	// pinocchio::urdf::buildModel(urdf_filename, model);
-	pinocchio::ModelTpl<double> model = model_double.cast<double>();
+	pinocchio::ModelTpl<float> model = model_double.cast<float>();
 
 	// // Create data required by the algorithms
-	DataTpl<double> data(model);
+	DataTpl<float> data(model);
 
 	// // Create the state and actuation models
-	boost::shared_ptr<crocoddyl::StateMultibodyTpl<double>> state =
-		boost::make_shared<crocoddyl::StateMultibodyTpl<double>>(
-			boost::make_shared<pinocchio::ModelTpl<double>>(model));
-	// boost::shared_ptr<pinocchio::ModelTpl<double>> robot_model_ptr = boost::make_shared<pinocchio::ModelTpl<double>>(robot_model);
-	// boost::shared_ptr<crocoddyl::StateMultibodyTpl<double>> state = boost::make_shared<crocoddyl::StateMultibodyTpl<double>>(robot_model_ptr);
-	// crocoddyl::StateMultibody state(robot_model_ptr);
-	// crocoddyl::StateMultibodyTpl<double> state_float = state.cast<double>();
+	boost::shared_ptr<crocoddyl::StateMultibodyTpl<float>> state =
+		boost::make_shared<crocoddyl::StateMultibodyTpl<float>>(
+			boost::make_shared<pinocchio::ModelTpl<float>>(model));
 
-	boost::shared_ptr<crocoddyl::ActuationModelFullTpl<double>> actuation =
-		boost::make_shared<crocoddyl::ActuationModelFullTpl<double>>(state);
+	boost::shared_ptr<crocoddyl::ActuationModelFullTpl<float>> actuation =
+		boost::make_shared<crocoddyl::ActuationModelFullTpl<float>>(state);
 
 	// Sample a random configuration
-	Eigen::VectorXd q = randomConfiguration(model);
+	Eigen::VectorXf q = randomConfiguration(model);
 	std::cout << "q: " << q.transpose() << std::endl;
 
 	// Perform the forward kinematics over the kinematic tree
@@ -98,10 +94,10 @@ int main(int argc, char **argv)
 	int goal_state = 0;
 
 	snprintf(eePos_traj_file_name, sizeof(eePos_traj_file_name), "testfiles/%d_%d_eepos.traj", start_state, goal_state);
-	std::vector<std::vector<double>> eePos_traj2d = readCSVToVecVec<double>(eePos_traj_file_name);
+	std::vector<std::vector<float>> eePos_traj2d = readCSVToVecVec<float>(eePos_traj_file_name);
 
 	snprintf(xu_traj_file_name, sizeof(xu_traj_file_name), "testfiles/%d_%d_traj.csv", start_state, goal_state);
-	std::vector<std::vector<double>> xu_traj2d = readCSVToVecVec<double>(xu_traj_file_name);
+	std::vector<std::vector<float>> xu_traj2d = readCSVToVecVec<float>(xu_traj_file_name);
 
 	if (eePos_traj2d.size() < knot_points)
 	{
@@ -109,12 +105,12 @@ int main(int argc, char **argv)
 		return 0;
 	}
 
-	std::vector<double> h_eePos_traj;
+	std::vector<float> h_eePos_traj;
 	for (const auto &vec : eePos_traj2d)
 	{
 		h_eePos_traj.insert(h_eePos_traj.end(), vec.begin(), vec.end());
 	}
-	std::vector<double> h_xu_traj;
+	std::vector<float> h_xu_traj;
 	for (const auto &xu_vec : xu_traj2d)
 	{
 		h_xu_traj.insert(h_xu_traj.end(), xu_vec.begin(), xu_vec.end());
@@ -128,34 +124,34 @@ int main(int argc, char **argv)
 	}
 
 	// // Initialize matrices Q and R for state and control tracking cost
-	Eigen::VectorXd Q_vec(NUM_STATES);
+	Eigen::VectorXf Q_vec(NUM_STATES);
 	Q_vec.fill(Q_COST);
 	
-	Eigen::VectorXd R_vec(NUM_CONTROLS);
+	Eigen::VectorXf R_vec(NUM_CONTROLS);
 	R_vec.fill(R_COST);
 
-	Eigen::VectorXd EE_penalty_vec(EE_DIM_POS);
+	Eigen::VectorXf EE_penalty_vec(EE_DIM_POS);
 	EE_penalty_vec.fill(EE_COST);
 	
-	boost::shared_ptr<crocoddyl::ActivationModelWeightedQuadTpl<double>> activation_model_state = 
-		boost::make_shared<crocoddyl::ActivationModelWeightedQuadTpl<double>>(Q_vec);
-	boost::shared_ptr<crocoddyl::ActivationModelWeightedQuadTpl<double>> activation_model_control = 
-		boost::make_shared<crocoddyl::ActivationModelWeightedQuadTpl<double>>(R_vec);
+	boost::shared_ptr<crocoddyl::ActivationModelWeightedQuadTpl<float>> activation_model_state = 
+		boost::make_shared<crocoddyl::ActivationModelWeightedQuadTpl<float>>(Q_vec);
+	boost::shared_ptr<crocoddyl::ActivationModelWeightedQuadTpl<float>> activation_model_control = 
+		boost::make_shared<crocoddyl::ActivationModelWeightedQuadTpl<float>>(R_vec);
 
 
 	// Initialize the cost models and action models
-	boost::shared_ptr<crocoddyl::CostModelSumTpl<double>> running_cost_model =
-		boost::make_shared<crocoddyl::CostModelSumTpl<double>>(state);
-	boost::shared_ptr<crocoddyl::CostModelSumTpl<double>> terminal_cost_model =
-		boost::make_shared<crocoddyl::CostModelSumTpl<double>>(state);
+	boost::shared_ptr<crocoddyl::CostModelSumTpl<float>> running_cost_model =
+		boost::make_shared<crocoddyl::CostModelSumTpl<float>>(state);
+	boost::shared_ptr<crocoddyl::CostModelSumTpl<float>> terminal_cost_model =
+		boost::make_shared<crocoddyl::CostModelSumTpl<float>>(state);
 
 	printf("initialized the cost models\n");
 
 	for (int t = 0; t < KNOT_POINTS; ++t)
 	{
-		Eigen::VectorXd x_ref_t(NUM_STATES);
-		Eigen::VectorXd u_ref_t(NUM_CONTROLS);
-		Eigen::VectorXd ee_pos_target(EE_DIM_POS);
+		Eigen::VectorXf x_ref_t(NUM_STATES);
+		Eigen::VectorXf u_ref_t(NUM_CONTROLS);
+		Eigen::VectorXf ee_pos_target(EE_DIM_POS);
 		// read into x_ref_t and u_ref_t from xu_traj2d
 		for (int i = 0; i < NUM_STATES; ++i)
 		{
@@ -168,39 +164,25 @@ int main(int argc, char **argv)
 			}
 		}
 
-		// Commenting out adding state cost for now, we don't penalize position in gpu-pcg-tracking, only velocity,
-		// and there doesn't seem to be a supported way to do that in crocoddyl
-		// boost::shared_ptr<crocoddyl::ResidualModelState> state_residual =
-		// 	boost::make_shared<crocoddyl::ResidualModelState>(state, x_ref_t);
-		// printf("initialized state_residual\n");
-		// boost::shared_ptr<crocoddyl::CostModelResidual> state_cost =
-		// 	boost::make_shared<crocoddyl::CostModelResidual>(state, state_residual);
-		// printf("initialized state_cost\n");
 
-		// running_cost_model->addCost("stateTrack" + std::to_string(t), state_cost, 1.);
+		boost::shared_ptr<crocoddyl::ActivationModelWeightedQuadTpl<float>> activation_ee = 
+			boost::make_shared<crocoddyl::ActivationModelWeightedQuadTpl<float>>(EE_penalty_vec);
 
-		// printf("Added state cost for knot point %d\n", t);
-		boost::shared_ptr<crocoddyl::ResidualModelStateTpl<double>> terminal_state_residual =
-			boost::make_shared<crocoddyl::ResidualModelStateTpl<double>>(state, x_ref_t); // 0: nu
-
-		boost::shared_ptr<crocoddyl::ActivationModelWeightedQuadTpl<double>> activation_ee = 
-			boost::make_shared<crocoddyl::ActivationModelWeightedQuadTpl<double>>(EE_penalty_vec);
-
-		boost::shared_ptr<crocoddyl::ResidualModelFrameTranslationTpl<double>> ee_residual =
-			boost::make_shared<crocoddyl::ResidualModelFrameTranslationTpl<double>>(state, model.getFrameId("iiwa_joint_7"), ee_pos_target);
+		boost::shared_ptr<crocoddyl::ResidualModelFrameTranslationTpl<float>> ee_residual =
+			boost::make_shared<crocoddyl::ResidualModelFrameTranslationTpl<float>>(state, model.getFrameId("iiwa_joint_7"), ee_pos_target);
 		printf("initialized state_residual\n");
-		boost::shared_ptr<crocoddyl::CostModelResidualTpl<double>> goal_tracking_xyz_cost =
-			boost::make_shared<crocoddyl::CostModelResidualTpl<double>>(state, activation_ee, ee_residual);
+		boost::shared_ptr<crocoddyl::CostModelResidualTpl<float>> goal_tracking_xyz_cost =
+			boost::make_shared<crocoddyl::CostModelResidualTpl<float>>(state, activation_ee, ee_residual);
 		printf("initialized state_cost\n");
 
 		running_cost_model->addCost("goalTrack" + std::to_string(t), goal_tracking_xyz_cost, 1.);
 
 		if (t < (KNOT_POINTS - 1)) {
 			// Add control cost if this is not the final knot point
-			boost::shared_ptr<crocoddyl::ResidualModelControlTpl<double>> control_residual =
-				boost::make_shared<crocoddyl::ResidualModelControlTpl<double>>(state, u_ref_t);
-			boost::shared_ptr<crocoddyl::CostModelResidualTpl<double>> control_cost =
-				boost::make_shared<crocoddyl::CostModelResidualTpl<double>>(state, control_residual);
+			boost::shared_ptr<crocoddyl::ResidualModelControlTpl<float>> control_residual =
+				boost::make_shared<crocoddyl::ResidualModelControlTpl<float>>(state, u_ref_t);
+			boost::shared_ptr<crocoddyl::CostModelResidualTpl<float>> control_cost =
+				boost::make_shared<crocoddyl::CostModelResidualTpl<float>>(state, control_residual);
 
 			running_cost_model->addCost("controlTrack" + std::to_string(t), control_cost, 0.001);
 
@@ -208,10 +190,14 @@ int main(int argc, char **argv)
 		}
 		else {
 			// Add terminal cost if this is final knot point
-			// boost::shared_ptr<crocoddyl::ResidualModelStateTpl<double>> terminal_state_residual =
-			// 	boost::make_shared<crocoddyl::ResidualModelStateTpl<double>>(state, x_ref_t); // 0: nu
-			// boost::shared_ptr<crocoddyl::CostModelResidualTpl<double>> terminal_state_cost =
-			// 	boost::make_shared<crocoddyl::CostModelResidualTpl<double>>(state, activation_model_state, terminal_state_residual);
+			// printf("Added state cost for knot point %d\n", t);
+			boost::shared_ptr<crocoddyl::ResidualModelStateTpl<float>> terminal_state_residual =
+				boost::make_shared<crocoddyl::ResidualModelStateTpl<float>>(state, x_ref_t);
+
+			// boost::shared_ptr<crocoddyl::ResidualModelStateTpl<float>> terminal_state_residual =
+			// 	boost::make_shared<crocoddyl::ResidualModelStateTpl<float>>(state, x_ref_t);
+			// boost::shared_ptr<crocoddyl::CostModelResidualTpl<float>> terminal_state_cost =
+			// 	boost::make_shared<crocoddyl::CostModelResidualTpl<float>>(state, activation_model_state, terminal_state_residual);
 			
 			// terminal_cost_model->addCost("stateTrack" + std::to_string(t), terminal_state_cost, 10000.);
 
@@ -221,22 +207,22 @@ int main(int argc, char **argv)
 		}
 	}
 
-	// // Create the action models
-	// boost::shared_ptr<crocoddyl::IntegratedActionModelEulerTpl<double>> running_model =
-	// 	boost::make_shared<crocoddyl::IntegratedActionModelEulerTpl<double>>(
-	// 		boost::make_shared<crocoddyl::DifferentialActionModelFreeFwdDynamicsTpl<double>>(state, actuation, running_cost_model),
+	// Create the action models
+	// boost::shared_ptr<crocoddyl::IntegratedActionModelEulerTpl<float>> running_model =
+	// 	boost::make_shared<crocoddyl::IntegratedActionModelEulerTpl<float>>(
+	// 		boost::make_shared<crocoddyl::DifferentialActionModelFreeFwdDynamicsTpl<float>>(state, actuation, running_cost_model),
 	// 		DT);
-	// boost::shared_ptr<crocoddyl::IntegratedActionModelEulerTpl<double>> terminal_model =
-	// 	boost::make_shared<crocoddyl::IntegratedActionModelEulerTpl<double>>(
-	// 		boost::make_shared<crocoddyl::DifferentialActionModelFreeFwdDynamicsTpl<double>>(state, actuation, terminal_cost_model),
+	// boost::shared_ptr<crocoddyl::IntegratedActionModelEulerTpl<float>> terminal_model =
+	// 	boost::make_shared<crocoddyl::IntegratedActionModelEulerTpl<float>>(
+	// 		boost::make_shared<crocoddyl::DifferentialActionModelFreeFwdDynamicsTpl<float>>(state, actuation, terminal_cost_model),
 	// 		DT);
 
 	// printf("initialized the action models\n");
 
 	// // Create the problem
-	// std::vector<boost::shared_ptr<crocoddyl::ActionModelAbstractTpl<double>>> running_models(
+	// std::vector<boost::shared_ptr<crocoddyl::ActionModelAbstractTpl<float>>> running_models(
 	// 	KNOT_POINTS, running_model);
-	// crocoddyl::ShootingProblemTpl<double> problem(x0, running_models, terminal_model);
+	// crocoddyl::ShootingProblemTpl<float> problem(x0, running_models, terminal_model);
 	// boost::shared_ptr<crocoddyl::ShootingProblem> problem_ptr = boost::make_shared<crocoddyl::ShootingProblem>(x0, running_models, terminal_model);
 
 
@@ -259,5 +245,5 @@ int main(int argc, char **argv)
     // // Print it
     // std::cout << "Final state: " << final_state << std::endl;
 
-	return 0;
+	// return 0;
 }
